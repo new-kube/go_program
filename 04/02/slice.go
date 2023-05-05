e038103c7593d4883511ba324d43c48b095841f8
stephon_young <stephon_young@163.com>
2023-05-05 22:27:13 +0800

    study slice replace in place

--- 04/02/slice.go
+++ 04/02/slice.go
@@ -85,7 +85,7 @@ func empty_and_nil() {
 	s = nil
 	// 无论是nil还是empty，语义都一样，所以归一处理。
 	if len(s) == 0 {
-		fmt.Printf("s is nil or empty, will return\n")
+		fmt.Printf("s is nil, will return\n")
 	}
 
 	// 如果slice为nil，那么for循环不会进入。这个也做了兼容处理
@@ -94,6 +94,16 @@ func empty_and_nil() {
 	}
 
 	fmt.Printf("not panic\n")
+
+	s = []int{}
+	// 空slice
+	if len(s) == 0 {
+		fmt.Printf("s is empty, will return\n")
+	}
+
+	for _, v := range s {
+		fmt.Printf("v=%d\n", v)
+	}
 }
 
 func append_int(x []int, y int) []int {
@@ -111,6 +121,10 @@ func append_int(x []int, y int) []int {
 		}
 		z = make([]int, zlen, zcap)
 		copy(z, x) // 内置的copy函数
+		// 拷贝内置函数：
+		// 1. 原型: copy(dst, src []T) int
+		// 2. 原理：将src中的元素拷贝到dst中，返回拷贝的元素个数。返回的拷贝元素个数为两个slice长度的最小值。
+		//    所以不存在panic的情况发生。即不会存在拷贝越界的情况发生。
 	}
 	z[len(x)] = y
 	return z
@@ -135,10 +149,91 @@ func slice_append() {
 			cap int
 		}
 	*/
+
+	// 注意：内置的append函数，通常可以追加一个或者多个元素，和append_int一样，如果容量不够，那么会重新分配一个底层数组。
+	// 所以需要注意，无论是append内置函数，还是自定义函数，如果有改变slice的指针、len、cap，都会触发底层的数组的重新的分配
+	// 所以，需要用显式的赋值。
+	// 如下：
+	var s []int
+	s = append(s, 1)
+	s = append(s, 2, 3)
+	s = append(s, []int{4, 5, 6}...) // ...表示将slice打散成多个元素。
+}
+
+func remove_null(s []string) []string {
+	i := 0
+	for _, v := range s {
+		if v != "" {
+			s[i] = v
+			i++
+		}
+	}
+	return s[:i]
+}
+
+func modify_in_place() {
+	// 就地修改。
+
+	data := []string{"one", "", "three"}
+	data = remove_null(data)
+	fmt.Printf("%q\n", data)
+
+	// panic: index out of range
+	//fmt.Printf("data[2]=%s\n", data[2])
+
+	data = data[:3] // extend len from 2 to 3
+	fmt.Printf("%q\n", data)
+
+}
+
+func slice_stack() {
+	// slice模拟栈的操作。
+
+	// 1. 弹出
+	pop := func(s []int) (int, []int) {
+		return s[len(s)-1], s[:len(s)-1]
+	}
+
+	// 2. 压入
+	push := func(s []int, x int) []int {
+		return append(s, x)
+	}
+
+	// 3. 移除，保持相对顺序
+	remove := func(s []int, i int) []int {
+		copy(s[i:], s[i+1:])
+		return s[:len(s)-1]
+	}
+
+	// 4. 移除，不保持相对顺序
+	remove2 := func(s []int, i int) []int {
+		s[i] = s[len(s)-1]
+		return s[:len(s)-1]
+	}
+
+	// 例子
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Printf("origin s=%v\n", s)
+
+	s = push(s, 6)
+	fmt.Printf("push s=%v\n", s)
+
+	v, s := pop(s)
+	fmt.Printf("pop v=%d, s=%v\n", v, s)
+
+	s = remove(s, 2)
+	fmt.Printf("remove s=%v\n", s)
+
+	s = remove2(s, 1)
+	fmt.Printf("remove2 s=%v\n", s)
+
 }
 
 func main() {
 	//slice_feature()
 	//empty_and_nil()
-	slice_append()
+	//slice_append()
+
+	//modify_in_place()
+	slice_stack()
 }
